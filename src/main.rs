13356fd9d5f4ed38c089d5b695f1e30f11f2dee8
efugier <mail@emilienfugier.net>
2024-04-25 11:53:36 +0200

    v1.2.1
    Better onboard and documentation fixes

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ const DEFAULT_PROMPT_NAME: &str = "default";
 #[command(
     name = "smartcat (sc)",
     author = "Emilien Fugier",
-    version = "1.2.0",
+    version = "1.2.1",
     about = "Putting a brain behind `cat`. CLI interface to bring language models in the Unix ecosystem 🐈‍⬛",
     long_about = None,
     after_help = "Examples:
