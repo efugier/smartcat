a3543ccfdd9649d7bb3debb7b2a58906abb617cc
efugier <mail@emilienfugier.net>
2024-11-04 10:14:37 +0100

    v2.1.0
    add azure openai to the supported APIs

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ const DEFAULT_PROMPT_NAME: &str = "default";
 #[command(
     name = "smartcat (sc)",
     author = "Emilien Fugier",
-    version = "2.0.0",
+    version = "2.1.0",
     about = "Putting a brain behind `cat`. CLI interface to bring language models in the Unix ecosystem 🐈‍⬛",
     long_about = None,
     after_help = "Examples:
